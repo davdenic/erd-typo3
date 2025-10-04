60b12d9f6a6b4ab39174a46a119ddbe724550c56
David Denicolo <davdenic@users.noreply.github.com>
2025-10-04 10:00:00 +0200

    feat: begin TYPO3 13 migration

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,10 +7,10 @@ $EM_CONF['erd'] = [
     'author' => 'David Denicolo',
     'author_email' => '',
     'state' => 'stable',
-    'version' => '2.0.0',
+    'version' => '3.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-12.4.99',
+            'typo3' => '13.4.0-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
